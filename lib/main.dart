4095deb46609f316453af3000abc64a6217fe7b1
NoeOnDev <alxg5516@gmail.com>
2024-11-20 04:12:46 -0600

    Eliminar lógica de navegación basada en el token de verificación; simplificar la inicialización de rutas en la aplicación

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/register_screen.dart';
 import 'screens/complete_registration_screen.dart';
 import 'screens/verify_account_screen.dart';
@@ -21,87 +20,72 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  Future<String?> _getInitialRoute() async {
-    final prefs = await SharedPreferences.getInstance();
-    final token = prefs.getString('token_user_verification');
-    return token != null ? '/home' : '/';
-  }
-
   @override
   Widget build(BuildContext context) {
     const String baseUrl =
         'https://8b6a-2806-262-3487-34f-ed35-164c-9e0a-6ac9.ngrok-free.app';
 
-    return FutureBuilder<String?>(
-      future: _getInitialRoute(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator();
-        } else {
-          return MaterialApp(
-            title: 'Conexer',
-            theme: ThemeData(
-              fontFamily: 'Hepta Slab',
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
-            ),
-            initialRoute: snapshot.data ?? '/',
-            debugShowCheckedModeBanner: false,
-            routes: {
-              '/': (context) =>
-                  LoginScreen(userService: UserService(baseUrl: baseUrl)),
-              '/select-register': (context) => const SelectRegisterScreen(),
-              '/register': (context) {
-                final args = ModalRoute.of(context)!.settings.arguments
-                    as Map<String, dynamic>;
-                return RegisterScreen(
-                  contactService: ContactService(baseUrl: baseUrl),
-                  role: args['role'],
-                );
-              },
-              '/complete-registration': (context) {
-                final args = ModalRoute.of(context)!.settings.arguments
-                    as Map<String, dynamic>;
-                return CompleteRegistrationScreen(
-                    contactId: args['contactId']!,
-                    role: args['role']!,
-                    userService: UserService(baseUrl: baseUrl));
-              },
-              '/verify-account': (context) {
-                final args = ModalRoute.of(context)!.settings.arguments
-                    as Map<String, dynamic>;
-                return VerifyAccountScreen(
-                    token: args['token']!,
-                    verifyService: VerifyService(baseUrl: baseUrl));
-              },
-              '/forgot-password': (context) => ForgotPasswordScreen(
-                    userService: UserService(baseUrl: baseUrl),
-                  ),
-              '/verify-password': (context) {
-                final args = ModalRoute.of(context)!.settings.arguments
-                    as Map<String, dynamic>;
-                return VerifyPasswordScreen(
-                    token: args['token']!,
-                    verifyService: VerifyService(baseUrl: baseUrl));
-              },
-              '/reset-password': (context) {
-                final args = ModalRoute.of(context)!.settings.arguments
-                    as Map<String, dynamic>;
-                return ResetPasswordScreen(
-                    jwtToken: args['jwtToken']!,
-                    userService: UserService(baseUrl: baseUrl));
-              },
-              '/verify-2fa': (context) {
-                final args = ModalRoute.of(context)!.settings.arguments
-                    as Map<String, dynamic>;
-                return Verify2FAScreen(
-                    token: args['token']!,
-                    verifyService: VerifyService(baseUrl: baseUrl));
-              },
-              '/home': (context) => const HomeScreen(),
-            },
+    return MaterialApp(
+      title: 'Conexer',
+      theme: ThemeData(
+        fontFamily: 'Hepta Slab',
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) =>
+            LoginScreen(userService: UserService(baseUrl: baseUrl)),
+        '/select-register': (context) => const SelectRegisterScreen(),
+        '/register': (context) {
+          final args = ModalRoute.of(context)!.settings.arguments
+              as Map<String, dynamic>;
+          return RegisterScreen(
+            contactService: ContactService(baseUrl: baseUrl),
+            role: args['role'],
           );
-        }
+        },
+        '/complete-registration': (context) {
+          final args = ModalRoute.of(context)!.settings.arguments
+              as Map<String, dynamic>;
+          return CompleteRegistrationScreen(
+              contactId: args['contactId']!,
+              role: args['role']!,
+              userService: UserService(baseUrl: baseUrl));
+        },
+        '/verify-account': (context) {
+          final args = ModalRoute.of(context)!.settings.arguments
+              as Map<String, dynamic>;
+          return VerifyAccountScreen(
+              token: args['token']!,
+              verifyService: VerifyService(baseUrl: baseUrl));
+        },
+        '/forgot-password': (context) => ForgotPasswordScreen(
+              userService: UserService(baseUrl: baseUrl),
+            ),
+        '/verify-password': (context) {
+          final args = ModalRoute.of(context)!.settings.arguments
+              as Map<String, dynamic>;
+          return VerifyPasswordScreen(
+              token: args['token']!,
+              verifyService: VerifyService(baseUrl: baseUrl));
+        },
+        '/reset-password': (context) {
+          final args = ModalRoute.of(context)!.settings.arguments
+              as Map<String, dynamic>;
+          return ResetPasswordScreen(
+              jwtToken: args['jwtToken']!,
+              userService: UserService(baseUrl: baseUrl));
+        },
+        '/verify-2fa': (context) {
+          final args = ModalRoute.of(context)!.settings.arguments
+              as Map<String, dynamic>;
+          return Verify2FAScreen(
+              token: args['token']!,
+              verifyService: VerifyService(baseUrl: baseUrl));
+        },
+        '/home': (context) => const HomeScreen(),
       },
     );
   }
